b05877f16bbaa963e0fcb77d90c0161bc718882a
sushantpande <we.sushant@gmail.com>
2018-05-31 15:18:18 +0530

    Create dataframe-query.scala

---
+++ dataframe-query.scala
@@ -0,0 +1,4 @@
+val parqfile = sqlContext.read.parquet("/user/pnda/result")
+parqfile.show()
+parqfile.filter("""src='louise.kitchen@enron.com'""").select("count").count()
+parqfile.filter("""src='louise.kitchen@enron.com'""").filter("""dst='louise.kitchen@enron.com'""").select("count").count()
