3ba977e4f9d7691f44172bdfdde7fcdf8e4a3e43
sushantpande <we.sushant@gmail.com>
2018-06-01 19:17:26 +0530

    Add comments

--- python-query.py
+++ python-query.py
@@ -1,8 +1,24 @@
 from graphframes import *
-sc.setCheckpointDir("/user/pnda/checkpoint")
-e = sqlContext.read.parquet("/user/pnda/result/edge")
+
+#load vertices
 v = sqlContext.read.parquet("/user/pnda/result/vertex")
+
+#load edges
+e = sqlContext.read.parquet("/user/pnda/result/edge")
+
+#run sample queries
 e.filter("""src='liz.taylor@enron.com'""").show()
 e.filter("""src='kalmeida@caiso.com'""").filter("""dst='chris.stokley@enron.com'""").show()
+
+#create graph
 g = GraphFrame(v,e)
+
+#get all paths between A and B
 g.bfs("id='neil.davies@enron.com'", "id = 'caroline.emmert@enron.com'").show()
+
+#connected components
+#sc.setCheckpointDir("/user/pnda/checkpoint")
+#result = g.connectedComponents()
+#result.select("id", "component").orderBy("component").show()
+#e.filter("""src='ken.lay-@enron.com'""").show()
+#e.filter("""src='all.worldwide@enron.com'""").show()
