7125c3bee697d80afe22f9582f7319de534f5a3a
sushantpande <we.sushant@gmail.com>
2018-06-01 18:56:21 +0530

    bug fix

--- python-query.py
+++ python-query.py
@@ -1,6 +1,6 @@
 from graphframes import *
 e = sqlContext.read.parquet("/user/pnda/result/edge")
-v = e = sqlContext.read.parquet("/user/pnda/result/vertex")
+v = sqlContext.read.parquet("/user/pnda/result/vertex")
 e.filter("""src='liz.taylor@enron.com'""").show()
 e.filter("""src='kalmeida@caiso.com'""").filter("""dst='chris.stokley@enron.com'""").show()
 g = GraphFrame(v,e)
